d7ce9ec88fbfd8c0923473535330e8b794aa5b6d
Diego Dukão <diegodukao@gmail.com>
2016-09-01 17:17:13 -0300

    showing numbers on display

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from kivy.app import App
 
 
 class Calkvlator(App):
-    pass
+
+    def print_number(self, number):
+        text = "{}{}".format(self.root.display.text, number)
+        self.root.display.text = text
+
 
 if __name__ == "__main__":
     Calkvlator().run()
